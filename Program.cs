af31fd7703d0b845b08cf016b88b2d9bb5374031
Alexander Nunez <xanderve@gmail.com>
2017-05-10 13:04:27 -0400

    Done

--- Program.cs
+++ Program.cs
@@ -4,9 +4,27 @@ namespace multiplicationTable
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Type a integer number");
+            int n = int.Parse(Console.ReadLine());
+
+            // Declaring variables to keep track of the position of the number to print in the matrix
+            int row;
+            int col;
+
+            // Looping and multiplicating each row by each column.                       
+            for(row=1;row<=n;row++)
+            {
+                for(col=1;col<=n;col++){
+                    Console.Write(row*col);
+                    if(col<5){
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
     }
 }
