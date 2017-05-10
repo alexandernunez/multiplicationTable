1509d5412a717cc162ac5707ed8fd9113cb78b4f
Alexander Nunez <xanderve@gmail.com>
2017-05-10 12:45:41 -0400

    starting

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace multiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
